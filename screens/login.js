a5756b2fc9cb50da3549a2b3edde21d18863178d
Rahul Singh <rahulsingh793@gmail.com>
2021-12-22 02:39:58 +0530

    skeleton phase 1

---
+++ screens/login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native-web'
+
+function login() {
+    return (
+        <View>
+            <Text>lOGIN SCREEN</Text>
+        </View>
+    )
+}
+
+export default login
---
+++ screens/welcome.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native-web'
+
+function welcome() {
+    return (
+        <View>
+<Text>Welcome Page</Text>
+        </View>
+            
+
+    )
+}
+
+export default welcome
